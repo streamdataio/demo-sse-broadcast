5310255256605831075a85b4667df92404c287c7
ctranxuan <ctranxuan@gmail.com>
2015-07-23 08:53:57 +0200

    feat(tomcat deployment): create a branch for tomcat deployment

--- src/main/java/demo/DemoSseBroadcastApplication.java
+++ src/main/java/demo/DemoSseBroadcastApplication.java
@@ -2,13 +2,20 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Streamdata.io - http://streamdata.io
  * Created by ctranxuan on 22/07/15.
  */
 @SpringBootApplication
-public class DemoSseBroadcastApplication {
+public class DemoSseBroadcastApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoSseBroadcastApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoSseBroadcastApplication.class, args);
